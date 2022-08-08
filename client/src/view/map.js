3dde34de64039fce9b4361651a2146f0ef09105f
Andres Rodriguez <andres6459@gmail.com>
2022-08-07 18:49:57 -0700

    Added Map API keys to a env file and added that fild to gitignore.

---
+++ client/src/view/map.js
@@ -0,0 +1,13 @@
+import ArcGISMap from "@arcgis/core/Map";
+import MapView from "@arcgis/core/views/MapView";
+import Graphic from "@arcgis/core/Graphic";
+
+import "@arcgis/core/assets/esri/themes/light/main.css";
+
+import { Text } from "@chakra-ui/react";
+
+const MapOfCameras = () => {
+  return <Text>Hello</Text>;
+};
+
+export default MapOfCameras;
