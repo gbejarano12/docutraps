43d30941eb231009dbff0d0e310e48b5ef6054a3
Andres Rodriguez <andres6459@gmail.com>
2022-08-07 19:10:41 -0700

    Corrected env setup

--- client/src/view/map.js
+++ client/src/view/map.js
@@ -1,13 +1,14 @@
 import ArcGISMap from "@arcgis/core/Map";
 import MapView from "@arcgis/core/views/MapView";
 import Graphic from "@arcgis/core/Graphic";
+import env from "react-dotenv";
 
 import "@arcgis/core/assets/esri/themes/light/main.css";
 
 import { Text } from "@chakra-ui/react";
 
 const MapOfCameras = () => {
-  return <Text>Hello</Text>;
+  return <Text>{"hello"}</Text>;
 };
 
 export default MapOfCameras;
