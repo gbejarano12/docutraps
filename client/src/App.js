2febdbe8b733843f9c3e811d04a5a417f2d23736
Gustavo Bejarano <bejarano_gustavo@yahoo.com>
2022-08-09 14:55:40 -0700

    test 6

--- client/src/App.js
+++ client/src/App.js
@@ -217,28 +217,36 @@ function MediaValetAuth() {
   const getToken = async () => {
     const params = {
       grant_type: 'authorization_code',
-      code: searchParams.get('code'),
+      code: '87gkm4eIngX9GKpGI4zQDgjG1rEteOfYa3DvPkIQrR8',
       client_id: '7f495f1f-21dc-4f9b-9071-4b56e5375e9f',
       redirect_uri: 'https://docutraps.azurewebsites.net/mediavalet/auth/callback', // <-- This is the url of the page they just redirected to (not including the querystring)
       client_secret: '86hVfmmct24ydSqAmBhdArCMw3fSz92kPL814ZWoeYw='
     };
 
+    var formBody = [];
+    for (var property in params) {
+      var encodedKey = encodeURIComponent(property);
+      var encodedValue = params[property];
+      formBody.push(encodedKey + "=" + encodedValue);
+    }
+    formBody = formBody.join("&");
+
     let tokenResponse = fetch('https://login.mediavalet.com/connect/token', 
         {
           method: 'POST',
           headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
             'Ocp-Apim-Subscription-Key': '03e0a3d8270a432d9ede6e2cfca073dd',
             Accept: '*/*',
             Host: 'login.mediavalet.com',
           },
-          body: JSON.stringify(params), // <-- This function turns the object into query param format eg. a=foo&b=barr
+          body: formBody, // <-- This function turns the object into query param format eg. a=foo&b=barr
           
         }
     );
 
     const response = await tokenResponse;
-    console.log(['Mediavalet token', response, params]);
+    console.log(['Mediavalet token', response, params, formBody]);
     const { access_token, expires_in } = response; 
   }
 
