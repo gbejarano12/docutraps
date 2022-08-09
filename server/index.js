370879beb832ccf3373a7c1b52baacd2b3076017
Gustavo Bejarano <bejarano_gustavo@yahoo.com>
2022-08-08 22:50:29 -0700

    test deploy 3

--- server/index.js
+++ server/index.js
@@ -24,6 +24,10 @@ app.get('/api', (req, res) => {
 	res.json({ message: 'Hello from server!' });
 });
 
+app.get("/*", (req, res) => {
+	res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
 app.listen(PORT, () => {
 	console.log(`Server listening on ${PORT}`);
 });
